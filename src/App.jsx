c7a4813516f6d6ef6d95d934b27ca2be1ae0f58f
sleepyded <144591527+deedGhost@users.noreply.github.com>
2024-07-12 10:34:16 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import WeatherApp from './Components/WeatherApp.jsx';
+function App() {
+  return (
+    <div className = 'App'>
+      <WeatherApp />
+    </div>
+  )
+}
+export default App
